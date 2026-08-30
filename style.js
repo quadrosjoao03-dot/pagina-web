130a45cc35f7f07e96bb69379f3f428582781f9e
quadrosjoao03-dot <quadros.joao03@escola.pr.gov.br>
2026-08-30 15:17:54 -0300

    3

--- style.js
+++ style.js
@@ -1,40 +1,12 @@
-const likeBtn = document.querySelector('.like-btn');
-const dislikeBtn = document.querySelector('.dislike-btn');
+function botaoClicado() {
+console.log("fui clicado");
+let texto = botao.querySelector("span");
 
-if (likeBtn && dislikeBtn) {
-  const likeCounter = likeBtn.querySelector('.contador');
-  const dislikeCounter = dislikeBtn.querySelector('.contador');
-
-  let likeTotal = 0;
-  let dislikeTotal = 0;
-  let votoAtual = null;
-
-  function atualizar() {
-    likeCounter.textContent = likeTotal;
-    dislikeCounter.textContent = dislikeTotal;
-  }
-
-  likeBtn.addEventListener('click', () => {
-    if (votoAtual === 'like') {
-      likeTotal--;
-      votoAtual = null;
-    } else {
-      if (votoAtual === 'dislike') dislikeTotal--;
-      likeTotal++;
-      votoAtual = 'like';
-    }
-    atualizar();
-  });
-
-  dislikeBtn.addEventListener('click', () => {
-    if (votoAtual === 'dislike') {
-      dislikeTotal--;
-      votoAtual = null;
-    } else {
-      if (votoAtual === 'like') likeTotal--;
-      dislikeTotal++;
-      votoAtual = 'dislike';
-    }
-    atualizar();
-  });
+if (curtiu === false) {
+texto.textContent++;
+curtiu = true;
+} else {
+texto.textContent--;
+curtiu = false;
+}
 }
